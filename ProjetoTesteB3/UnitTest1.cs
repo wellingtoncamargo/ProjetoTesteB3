0dda01b0806f88dd08e841ff955c5b8ac4d4525c
Wellington Camargo <wellingtonsc@hotmail.com>
2025-04-02 19:51:55 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoTesteB3/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProjetoTesteB3
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
