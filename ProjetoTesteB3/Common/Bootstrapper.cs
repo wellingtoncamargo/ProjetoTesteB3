ce308083fcbed3103d08087bc06e2e98e4af8382
Wellington Camargo <wellingtonsc@hotmail.com>
2025-04-03 12:00:07 -0300

    Criando bootstrapper

---
+++ ProjetoTesteB3/Common/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using BoDi;
+
+namespace ProjetoTesteB3.Common
+{
+
+    public class Bootstrapper : IDisposable
+    {
+        protected IWebDriver Driver { get; }
+
+        public Bootstrapper(IObjectContainer objectContainer)
+        {
+            objectContainer.RegisterInstanceAs(new ChromeDriver(), typeof(IWebDriver));
+            Driver = objectContainer.Resolve<IWebDriver>();
+        }
+
+        public void Dispose()
+        {
+            Driver?.Quit();
+            Driver?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
